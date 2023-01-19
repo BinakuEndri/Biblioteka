4e07e2249ea33a1c68db822a302f62631c734d77
Adriatik Krasniqi <adriatik.krasniqi2@student.uni-pr.edu>
2023-01-19 21:22:43 +0100

    Updating seats

--- Script/hover.js
+++ Script/hover.js
@@ -17,13 +17,13 @@ window.addEventListener("scroll", function () {
 
 
 var reserve = {
-  // (A) INIT
+  
   init : () => {
     // (A1) GET LAYOUT WRAPPER
     let layout = document.getElementById("layout1");
  
     // (A2) GENERATE SEATS
-    for (let i=65; i<=71; i++) { for (let j=1; j<=4; j++) {
+    for (let i=65; i<=72; i++) { for (let j=1; j<=8; j++) {
       let seat = document.createElement("div");
       seat.innerHTML = String.fromCharCode(i) + j;
       seat.className = "seat";
@@ -34,7 +34,7 @@ var reserve = {
     // (A3) FOR DEMO ONLY - RANDOM TAKEN SEATS
     let all = document.querySelectorAll("#layout1 .seat"),
         len = all.length - 1, rnd = [];
-    while (rnd.length != 3) {
+    while (rnd.length != 8) {
       let r = Math.floor(Math.random() * len);
       if (!rnd.includes(r)) { rnd.push(r); }
     }
@@ -55,27 +55,29 @@ var reserve = {
     let selected = document.querySelectorAll("#layout1 .selected");
  
     // (C2) ERROR!
-    if (selected.length == 0) { alert("No seats selected."); }
+    if (selected.length == 0) { alert("Duhet të zgjedhni karrige!"); }
  
     // (C3) SELECTED SEATS
     else {
       // (C3-1) GET SELECTED SEAT NUMBERS
       let seats = [];
       for (let s of selected) { seats.push(s.innerHTML); }
- 
-      // (C3-2) DO SOMETHING WITH IT...
-      let data = new FormData();
-      data.append("seats", JSON.stringify(seats));
-      data.append("name", "JON DOE");
-      data.append("email", "JON@DOE.COM");
-      fetch("4-dummy.php", {
-        method: "POST",
-        body : data
-      })
-      .then(res => res.text())
-      .then((txt) => { console.log(txt); });
     }
   }
 };
  
 window.addEventListener("DOMContentLoaded", reserve.init);
+
+
+function animateButton() {
+  var button = document.getElementById("save");
+  button.classList.add("animate");
+  setTimeout(function(){
+    button.classList.remove("animate");
+  }, 300);
+}
+
+document.getElementById("save").addEventListener("click", function(){
+  animateButton();
+  reserve.save();
+});
